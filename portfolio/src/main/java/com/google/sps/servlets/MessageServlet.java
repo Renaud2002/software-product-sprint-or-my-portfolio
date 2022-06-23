c9bceedd39985e5c3c06c96c9e74764b1a638b8e
Fred <renaudnoubieptie@gmail.com>
2022-06-23 03:20:24 +0000

    add gson convertion in MessageServlet.java

--- portfolio/src/main/java/com/google/sps/servlets/MessageServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/MessageServlet.java
@@ -16,8 +16,12 @@ package com.google.sps.servlets;
 
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.lang.reflect.Type;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -29,45 +33,33 @@ public final class  MessageServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Arraylist with messages
-    ArrayList<String> msg = new ArrayList<String>();
+    //List with messages
+    List<String> msg = new ArrayList<String>();
     msg.add("I am from Cameroon");
     msg.add("I like anime");
     msg.add("I am in college");
    
 
     // Convert the server stats to JSON
-    String json = convertToJson(msg);
+    String json = convertToJsonUsingGson(msg);
 
     // Send the JSON as the response
     response.setContentType("application/json;");
     response.getWriter().println(json);
   }
 
-  /**
-   * Converts a ServerStats instance into a JSON string using manual String concatentation.
-   */
-  private String convertToJson(ArrayList message) {
-    String json = "{";
-    json += "\"message1\": ";
-    json += "\"" + message.get(0) + "\"";
-    json += ", ";
-    json += "\"message2\": ";
-    json += "\"" + message.get(1) + "\"";
-    json += ", ";
-    json += "\"message3\": ";
-    json += "\"" + message.get(2) + "\"";
-    json += "}";
-    return json;
-  }
+ 
+  // converting using gson
+  private String convertToJsonUsingGson(List msg) {
+    // Gson gson = new Gson();
+    // String json = gson.toJson(serverStats);
+    // return json;
 
-  /**
-   * Converts a ServerStats instance into a JSON string using the Gson library. Note: We first added
-   * the Gson library dependency to pom.xml.
-   */
-  private String convertToJsonUsingGson(ArrayList serverStats) {
+    Type listType = new TypeToken<List<String>>() {}.getType();
+
     Gson gson = new Gson();
-    String json = gson.toJson(serverStats);
+    String json = gson.toJson(msg, listType);
+
     return json;
   }
 }
